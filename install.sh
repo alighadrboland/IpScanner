9226d4dbbcaad9d2672fd5a069a6c3e8345810f0
ÐΛɌ₭ᑎΞ𐒡𐒡 <104245967+mansor427@users.noreply.github.com>
2024-04-29 18:58:19 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -335,7 +335,7 @@ endipresult() {
     echo ""
     echo -e "${green} Saved in result.csv${rest}"
     echo ""
-    echo -e "${BRed}  💛Don't Forget SHΞЯVIN™💛     ${rest}"
+    echo -e "${BRed}${IRed}  💛Don't Forget SHΞЯVIN™💛     ${rest}"
     echo ""
     if [ "$Endip_v4" ]; then
         echo -e "${purple}************************************${rest}"
@@ -360,9 +360,9 @@ endipresult() {
 }
 
 clear
-echo -e "${blue}#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#${rest}"
+echo -e "${blue}#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#${rest}"
 echo -e "${blue}#+${cyan} By --> ÐΛɌ₭ᑎΞ𐒡𐒡 ${blue}# ${red}🧡Don't Forget SHΞЯVIN™🧡${blue} +#${rest}"
-echo -e "${blue}#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#${rest}"
+echo -e "${blue}#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#+#${rest}"
 echo ""
 echo -e "${purple}*********************${rest}"
 echo -e "${purple}* ${green}Endpoint Scanner ${purple} *${rest}"
