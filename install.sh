e93de7b52d4400e0a92727953d93a9981e425404
ÐΛɌ₭ᑎΞ𐒡𐒡 <104245967+mansor427@users.noreply.github.com>
2024-04-29 18:22:03 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,56 @@ cyan='\033[0;36m'
 white='\033[0;37m'
 rest='\033[0m'
 
+# Bold
+BBlack="\[\033[1;30m\]"       # Black
+BRed="\[\033[1;31m\]"         # Red
+BGreen="\[\033[1;32m\]"       # Green
+BYellow="\[\033[1;33m\]"      # Yellow
+BBlue="\[\033[1;34m\]"        # Blue
+BPurple="\[\033[1;35m\]"      # Purple
+BCyan="\[\033[1;36m\]"        # Cyan
+BWhite="\[\033[1;37m\]"       # White
+
+# Underline
+UBlack="\[\033[4;30m\]"       # Black
+URed="\[\033[4;31m\]"         # Red
+UGreen="\[\033[4;32m\]"       # Green
+UYellow="\[\033[4;33m\]"      # Yellow
+UBlue="\[\033[4;34m\]"        # Blue
+UPurple="\[\033[4;35m\]"      # Purple
+UCyan="\[\033[4;36m\]"        # Cyan
+UWhite="\[\033[4;37m\]"       # White
+
+# Background
+On_Black="\[\033[40m\]"       # Black
+On_Red="\[\033[41m\]"         # Red
+On_Green="\[\033[42m\]"       # Green
+On_Yellow="\[\033[43m\]"      # Yellow
+On_Blue="\[\033[44m\]"        # Blue
+On_Purple="\[\033[45m\]"      # Purple
+On_Cyan="\[\033[46m\]"        # Cyan
+On_White="\[\033[47m\]"       # White
+
+# High Intensty
+IBlack="\[\033[0;90m\]"       # Black
+IRed="\[\033[0;91m\]"         # Red
+IGreen="\[\033[0;92m\]"       # Green
+IYellow="\[\033[0;93m\]"      # Yellow
+IBlue="\[\033[0;94m\]"        # Blue
+IPurple="\[\033[0;95m\]"      # Purple
+ICyan="\[\033[0;96m\]"        # Cyan
+IWhite="\[\033[0;97m\]"       # White
+
+# Bold High Intensty
+BIBlack="\[\033[1;90m\]"      # Black
+BIRed="\[\033[1;91m\]"        # Red
+BIGreen="\[\033[1;92m\]"      # Green
+BIYellow="\[\033[1;93m\]"     # Yellow
+BIBlue="\[\033[1;94m\]"       # Blue
+BIPurple="\[\033[1;95m\]"     # Purple
+BICyan="\[\033[1;96m\]"       # Cyan
+BIWhite="\[\033[1;97m\]"      # White
+
 case "$(uname -m)" in
 	x86_64 | x64 | amd64 )
 	    cpu=amd64
@@ -283,8 +333,9 @@ endipresult() {
     Endip_v6=$(cat result.csv | grep -oE "\[.*\]:[0-9]+" | head -n 1)
     delay=$(cat result.csv | grep -oE "[0-9]+ ms|timeout" | head -n 1)
     echo ""
-    echo -e "${green}Results Saved in result.csv${rest}"
-    echo -e "${red} فراموش نکنیم SHΞЯVIN™ ${rest}"
+    echo -e "${green} Saved in result.csv${rest}"
+    echo ""
+    echo -e "${red} ${BRed}           Don't Forgot SHΞЯVIN™     ${rest}"
     echo ""
     if [ "$Endip_v4" ]; then
         echo -e "${purple}************************************${rest}"
@@ -309,7 +360,7 @@ endipresult() {
 }
 
 clear
-echo -e "${cyan}By --> ÐΛɌ₭ᑎΞ𐒡𐒡 * MANSOR427 * ${rest}"
+echo -e "${cyan}By --> ÐΛɌ₭ᑎΞ𐒡𐒡  <* Don't Forgot SHΞЯVIN™ *> ${rest}"
 echo ""
 echo -e "${purple}*********************${rest}"
 echo -e "${purple}* ${green}Endpoint Scanner ${purple} *${rest}"
