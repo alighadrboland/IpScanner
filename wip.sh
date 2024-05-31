38c44a2099c6c6758f9e0f8ca9f045e5333d5a02
ÐΛɌ₭ᑎΞ𐒡𐒡 <104245967+mansor427@users.noreply.github.com>
2024-05-31 23:46:43 +0330

    Add files via upload

---
+++ wip.sh
@@ -0,0 +1,204 @@
+#!/bin/bash
+
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+PLAIN='\033[0m'
+
+red() {
+	echo -e "\033[31m\033[01m$1\033[0m"
+}
+
+green() {
+	echo -e "\033[32m\033[01m$1\033[0m"
+}
+
+yellow() {
+	echo -e "\033[33m\033[01m$1\033[0m"
+}
+
+# Select client CPU architecture
+archAffix(){
+    case "$(uname -m)" in
+        i386 | i686 ) echo '386' ;;
+        x86_64 | amd64 ) echo 'amd64' ;;
+        armv8 | arm64 | aarch64 ) echo 'arm64' ;;
+        s390x ) echo 's390x' ;;
+        * ) red "Unsupported CPU architecture!" && exit 1 ;;
+    esac
+}
+
+endpointyx() {
+	wget https://raw.githubusercontent.com/mansor427/IpScanner/main/wip/warp-linux-$(archAffix) -O warp
+
+   ulimit -n 102400
+
+	chmod +x warp && ./warp >/dev/null 2>&1
+
+	green "The current optimal Endpoint IP results are as follows and have been saved to result.csv:"
+	cat result.csv | awk -F, '$3!="timeout ms" {print} ' | sort -t, -nk2 -nk3 | uniq | head -11 | awk -F, '{print "Endpoint "$1" Packet loss rate "$2" Average delay "$3}'
+	echo ""
+	yellow "How to use it:"
+	yellow "1. Replace the default Endpoint IP of the WireGuard node: engage.cloudflareclient.com:2408 with the optimal Endpoint IP of the local network"
+
+	rm -f warp ip.txt
+}
+
+endpoint4() {
+
+	n=0
+	iplist=100
+	while true; do
+		temp[$n]=$(echo 162.159.192.$(($RANDOM % 256)))
+		n=$(($n + 1))
+		if [ $n -ge $iplist ]; then
+			break
+		fi
+		temp[$n]=$(echo 162.159.193.$(($RANDOM % 256)))
+		n=$(($n + 1))
+		if [ $n -ge $iplist ]; then
+			break
+		fi
+		temp[$n]=$(echo 162.159.195.$(($RANDOM % 256)))
+		n=$(($n + 1))
+		if [ $n -ge $iplist ]; then
+			break
+		fi
+		temp[$n]=$(echo 162.159.204.$(($RANDOM % 256)))
+		n=$(($n + 1))
+		if [ $n -ge $iplist ]; then
+			break
+		fi
+		temp[$n]=$(echo 188.114.96.$(($RANDOM % 256)))
+		n=$(($n + 1))
+		if [ $n -ge $iplist ]; then
+			break
+		fi
+		temp[$n]=$(echo 188.114.97.$(($RANDOM % 256)))
+		n=$(($n + 1))
+		if [ $n -ge $iplist ]; then
+			break
+		fi
+		temp[$n]=$(echo 188.114.98.$(($RANDOM % 256)))
+		n=$(($n + 1))
+		if [ $n -ge $iplist ]; then
+			break
+		fi
+		temp[$n]=$(echo 188.114.99.$(($RANDOM % 256)))
+		n=$(($n + 1))
+		if [ $n -ge $iplist ]; then
+			break
+		fi
+	done
+	while true; do
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
+			break
+		else
+			temp[$n]=$(echo 162.159.192.$(($RANDOM % 256)))
+			n=$(($n + 1))
+		fi
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
+			break
+		else
+			temp[$n]=$(echo 162.159.193.$(($RANDOM % 256)))
+			n=$(($n + 1))
+		fi
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
+			break
+		else
+			temp[$n]=$(echo 162.159.195.$(($RANDOM % 256)))
+			n=$(($n + 1))
+		fi
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
+			break
+		else
+			temp[$n]=$(echo 162.159.204.$(($RANDOM % 256)))
+			n=$(($n + 1))
+		fi
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
+			break
+		else
+			temp[$n]=$(echo 188.114.96.$(($RANDOM % 256)))
+			n=$(($n + 1))
+		fi
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
+			break
+		else
+			temp[$n]=$(echo 188.114.97.$(($RANDOM % 256)))
+			n=$(($n + 1))
+		fi
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
+			break
+		else
+			temp[$n]=$(echo 188.114.98.$(($RANDOM % 256)))
+			n=$(($n + 1))
+		fi
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
+			break
+		else
+			temp[$n]=$(echo 188.114.99.$(($RANDOM % 256)))
+			n=$(($n + 1))
+		fi
+	done
+
+	echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u >ip.txt
+
+	endpointyx
+}
+
+endpoint6() {
+	n=0
+	iplist=100
+	while true; do
+		temp[$n]=$(echo [2606:4700:d0::$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2)))])
+		n=$(($n + 1))
+		if [ $n -ge $iplist ]; then
+			break
+		fi
+		temp[$n]=$(echo [2606:4700:d1::$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2)))])
+		n=$(($n + 1))
+		if [ $n -ge $iplist ]; then
+			break
+		fi
+	done
+	while true; do
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
+			break
+		else
+			temp[$n]=$(echo [2606:4700:d0::$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2)))])
+			n=$(($n + 1))
+		fi
+		if [ $(echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u | wc -l) -ge $iplist ]; then
+			break
+		else
+			temp[$n]=$(echo [2606:4700:d1::$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2))):$(printf '%x\n' $(($RANDOM * 2 + $RANDOM % 2)))])
+			n=$(($n + 1))
+		fi
+	done
+
+	echo ${temp[@]} | sed -e 's/ /\n/g' | sort -u >ip.txt
+
+	endpointyx
+}
+
+menu() {
+	clear
+	echo "∆∆∆∆∆∆∆∆∆∆∆∆∆∆"
+	echo -e "∆     ${RED}WARP  IP      ∆"
+	echo -e "∆    ${GREEN}ÐΛɌ₭ᑎΞ𐒡𐒡      ∆"
+	echo "∆∆∆∆∆∆∆∆∆∆∆∆∆∆"
+	echo ""
+	echo -e " ${GREEN}[1].${PLAIN} WARP IPv4 "
+	echo -e " ${GREEN}[2].${PLAIN} WARP IPv6"
+	echo " ••••••••••••••••••••••••-"
+	echo -e " ${GREEN}0.${PLAIN} Exit"
+	echo ""
+	read -rp "Please enter number [0-2]: " menuInput
+	case $menuInput in
+	2) endpoint6 ;;
+	0) exit 1 ;;
+	*) endpoint4 ;;
+	esac
+}
+
+menu
